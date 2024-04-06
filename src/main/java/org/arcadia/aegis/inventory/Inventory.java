cc2824d93b4709e99de78a6123fe341f6eb8bb29
TomGieben <t.gieben@outlook.com>
2024-04-06 09:56:01 +0200

    Inventory added

--- src/main/java/org/arcadia/aegis/inventory/Inventory.java
+++ src/main/java/org/arcadia/aegis/inventory/Inventory.java
@@ -1,4 +1,38 @@
 package org.arcadia.aegis.inventory;
 
 public class Inventory {
+    private ArrayList<InventoryItem> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public Inventory(ArrayList<InventoryItem> items) {
+        this.items = items;
+    }
+
+    public ArrayList<InventoryItem> all() {
+        return this.items;
+    }
+
+    public void store(InventoryItem item) {
+        this.items.add(item);
+    }
+
+    public boolean destroy(int index) {
+        if (index >= 0 && index < this.items.size()) {
+            this.items.remove(index);
+            return true;
+        }
+
+        return false;
+    }
+
+    public InventoryItem find(int index) {
+        if (index >= 0 && index < this.items.size()) {
+            return this.items.get(index);
+        }
+
+        return null;
+    }
 }
