ee0bf4ce85a8778ee1a94e2aad17f82a92791c90
XaderGator <xandergeurts@hotmail.nl>
2024-04-06 10:07:20 +0200

    Added bar and player classes

--- src/main/java/org/arcadia/aegis/objects/Bar.java
+++ src/main/java/org/arcadia/aegis/objects/Bar.java
@@ -1,4 +1,59 @@
 package org.arcadia.aegis.objects;
 
-public class Bar {
+import com.github.hanyaeger.api.entities.Collided;
+import com.github.hanyaeger.api.entities.Collider;
+import javafx.scene.Node;
+import org.arcadia.aegis.inventory.Inventory;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Bar implements Collided {
+    private Inventory inventory;
+    private String imagePath;
+    private int locationX;
+    private int locationY;
+
+    public Bar(String imagePath, int x, int y) {
+        this.imagePath = imagePath;
+        this.locationX = x;
+        this.locationY = y;
+    }
+    public Bar(String imagePath, int x, int y, Inventory inventory) {
+        this.imagePath = imagePath;
+        this.locationX = x;
+        this.locationY = y;
+        this.inventory = inventory;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public void viewBar(){
+        return;
+    }
+
+    @Override
+    public void onCollision(List<Collider> list) {
+
+    }
+
+    @Override
+    public Optional<? extends Node> getNode() {
+        return Optional.empty();
+    }
 }
+
--- src/main/java/org/arcadia/aegis/objects/Player.java
+++ src/main/java/org/arcadia/aegis/objects/Player.java
@@ -1,4 +1,84 @@
 package org.arcadia.aegis.objects;
 
-public class Player {
+import com.github.hanyaeger.api.entities.Collider;
+import javafx.scene.Node;
+import org.arcadia.aegis.enums.InfluenceType;
+import org.arcadia.aegis.game.Wallet;
+import org.arcadia.aegis.inventory.Inventory;
+
+import java.util.Optional;
+
+public class Player implements Collider {
+
+    private String name;
+    private Wallet wallet;
+    private Inventory inventory;
+    private String avatarPath;
+    private int locationX;
+    private int locationY;
+    private float influence;
+
+    public Player(String name, String avatarPath) {
+        this.name = name;
+        this.avatarPath = avatarPath;
+    }
+
+    public Player(String name, String avatarPath, Wallet wallet, Inventory inventory) {
+        this.name = name;
+        this.avatarPath = avatarPath;
+        this.wallet = wallet;
+        this.inventory = inventory;
+    }
+
+    public void setLocation(int x, int y) {
+        this.setLocationX(x);
+        this.setLocationY(y);
+    }
+
+    public void setLocationX(int locationX) {
+        this.locationX = locationX;
+    }
+
+    public void setLocationY(int locationY) {
+        this.locationY = locationY;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public String getAvatarPath() {
+        return avatarPath;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public float updateInfluence(float influence, InfluenceType influenceType) {
+        if (influenceType == InfluenceType.NEGATIVE) {
+            this.influence -= influence;
+        }else if (influenceType == InfluenceType.POSITIVE) {
+            this.influence += influence;
+        }
+
+        return this.influence;
+    }
+
+    public float getInfluence() {
+        return influence;
+    }
+
+    @Override
+    public Optional<? extends Node> getNode() {
+        return Optional.empty();
+    }
 }
