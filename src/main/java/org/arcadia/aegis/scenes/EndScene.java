1d8cc8d986557a5d513c992a1f7fa73b7dac7316
TomGieben <t.gieben@outlook.com>
2024-04-18 09:50:56 +0200

    Start scene added

--- src/main/java/org/arcadia/aegis/scenes/EndScene.java
+++ src/main/java/org/arcadia/aegis/scenes/EndScene.java
@@ -10,12 +10,12 @@ import javafx.scene.text.FontWeight;
 import org.arcadia.aegis.App;
 import org.arcadia.aegis.entities.buttons.StartButton;
 
-public class TitleScene extends DynamicScene {
+public class EndScene extends DynamicScene {
     final private String audioPath = "sounds/background_music.mp3";
     final private String backgroundPath = "backgrounds/titlescreen.jpg";
     final private App app;
 
-    public TitleScene(App app){
+    public EndScene(App app){
         this.app = app;
     }
     @Override
@@ -26,24 +26,13 @@ public class TitleScene extends DynamicScene {
 
     @Override
     public void setupEntities() {
-        this.renderStartButton();
         this.renderTitle();
-    }
-
-    private void renderStartButton() {
-        StartButton startButton = new StartButton(
-                this.app,
-                new Coordinate2D(getWidth() / 2, getHeight() / 2)
-        );
-
-        startButton.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        addEntity(startButton);
     }
 
     private void renderTitle() {
         TextEntity title = new TextEntity(
             new Coordinate2D(getWidth() / 2, getHeight()/ 3),
-            getStage().getTitle()
+            "Je hebt gewonnen"
         );
 
         title.setAnchorPoint(AnchorPoint.TOP_CENTER);
