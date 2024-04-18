51f6f23ed0ee0cb66d35e9ce186fcac4d03d5a08
TomGieben <t.gieben@outlook.com>
2024-04-18 11:36:02 +0200

    Playername fix

--- src/main/java/org/arcadia/aegis/App.java
+++ src/main/java/org/arcadia/aegis/App.java
@@ -2,6 +2,7 @@ package org.arcadia.aegis;
 
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
+import org.arcadia.aegis.objects.Player;
 import org.arcadia.aegis.scenes.GameScene;
 import org.arcadia.aegis.scenes.TitleScene;
 
@@ -11,6 +12,7 @@ import org.arcadia.aegis.scenes.TitleScene;
  */
 public class App extends YaegerGame
 {
+    private String playerName = "";
     public static void main( String[] args )
     {
         launch(args);
@@ -27,5 +29,13 @@ public class App extends YaegerGame
         addScene(0, new TitleScene(this));
         addScene(1, new GameScene(this));
 //        addScene(2, new EndScene(this));
+    }
+
+    public void setPlayerName(String name) {
+        this.playerName = name;
+    }
+
+    public String getPlayerName() {
+        return this.playerName;
     }
 }
--- src/main/java/org/arcadia/aegis/scenes/TitleScene.java
+++ src/main/java/org/arcadia/aegis/scenes/TitleScene.java
@@ -2,17 +2,25 @@ package org.arcadia.aegis.scenes;
 
 import com.github.hanyaeger.api.AnchorPoint;
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.YaegerEntity;
 import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import org.arcadia.aegis.App;
 import org.arcadia.aegis.entities.buttons.StartButton;
 
-public class TitleScene extends DynamicScene {
+import java.util.Set;
+
+public class TitleScene extends DynamicScene implements KeyListener {
     final private String audioPath = "sounds/background_music.mp3";
     final private String backgroundPath = "backgrounds/titlescreen.jpg";
+
+    private boolean prevWasEmpty = true;
     final private App app;
 
     public TitleScene(App app){
@@ -28,6 +36,7 @@ public class TitleScene extends DynamicScene {
     public void setupEntities() {
         this.renderStartButton();
         this.renderTitle();
+        this.renderNameInput();
     }
 
     private void renderStartButton() {
@@ -42,7 +51,7 @@ public class TitleScene extends DynamicScene {
 
     private void renderTitle() {
         TextEntity title = new TextEntity(
-            new Coordinate2D(getWidth() / 2, getHeight()/ 3),
+            new Coordinate2D(getWidth() / 2, getHeight() / 3),
             getStage().getTitle()
         );
 
@@ -51,5 +60,63 @@ public class TitleScene extends DynamicScene {
         title.setFont(Font.font("Roboto", FontWeight.BOLD, 60));
 
         addEntity(title);
+    }
+
+    private void renderNameInput() {
+        TextEntity input = new TextEntity(
+            new Coordinate2D(getWidth() / 2, 500),
+            "Voer je naam in:"
+        );
+
+        input.setAnchorPoint(AnchorPoint.BOTTOM_CENTER);
+        input.setFill(Color.WHITESMOKE);
+        input.setFont(Font.font("Roboto", FontWeight.BOLD, 20));
+
+        addEntity(input);
+    }
+
+    private void renderPlayerName() {
+        //TODO
+        String name = this.app.getPlayerName();
+
+        TextEntity name = new TextEntity(
+                new Coordinate2D(getWidth() / 2, 600),
+                name
+        );
+
+        name.setAnchorPoint(AnchorPoint.BOTTOM_CENTER);
+        name.setFill(Color.WHITESMOKE);
+        name.setFont(Font.font("Roboto", FontWeight.BOLD, 20));
+
+        addEntity(name);
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> set) {
+        String currentName = this.app.getPlayerName();
+        StringBuilder playerNameBuilder = new StringBuilder(currentName);
+
+        if(!this.prevWasEmpty) {
+            this.prevWasEmpty = set.isEmpty();
+        }
+
+        for (KeyCode keyCode : set) {
+            if (keyCode != KeyCode.BACK_SPACE) {
+                if(this.prevWasEmpty) {
+                    playerNameBuilder.append(keyCode);
+                    this.prevWasEmpty = false;
+                }
+            } else {
+                if (playerNameBuilder.length() > 0) {
+                    playerNameBuilder.deleteCharAt(playerNameBuilder.length() - 1);
+                }
+            }
+        }
+
+        String playerName = playerNameBuilder.toString();
+
+        this.app.setPlayerName(playerName);
+
+        this.renderPlayerName();
     }
 }
